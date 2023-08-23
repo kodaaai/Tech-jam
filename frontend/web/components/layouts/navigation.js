37bf7710a064ef7f23edefefd3f38f8d07e0445a
大城昂大 <oshirokodai@ooshirokoudainoMacBook-Pro.local>
2023-08-24 08:11:45 +0900

    コンポーネント化および見た目変更

--- frontend/web/components/layouts/navigation.js
+++ frontend/web/components/layouts/navigation.js
@@ -4,11 +4,11 @@ export default function Navigation() {
     return (
         <div>
             <header className="flex items-center justify-between py-4 md:py-8">
-                <Link href="#" className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl">
-                <a className="inline-flex items-center gap-2.5 text-2xl font-bold text-red-300 md:text-3xl" aria-label="logo">Qikoo</a>
+                <Link href="/" className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl">
+                <a className="inline-flex items-center gap-2.5 text-2xl font-bold text-indigo-500 md:text-3xl" aria-label="logo">Qikoo</a>
                 </Link>
                  <nav className="hidden gap-12 lg:flex">
-                    <Link href="">
+                    <Link href="http://0.0.0.0:8000">
                         <a className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">ホーム</a>
                     </Link>
                     <Link href="">
--- frontend/web/pages/index.js
+++ frontend/web/pages/index.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Link from 'next/link';
+import SearchBox from "../components/layouts/search_box"
 
 // import { getAllPostsData } from '../lib/posts'; //関数はこのような表記？
 
@@ -27,19 +28,23 @@ export default function Home() {
           Qikoo
         </title>
       </Head>
-      <div className='flex flex-wrap -m-4 mb-5'>
-        <div className='text-lg mb-3'>質問一覧</div>
-      </div>
+      <SearchBox />
+      <div class="bg-white py-6 sm:py-8 lg:py-12">
+        <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <p class="mb-2 font-semibold text-indigo-500 md:mb-3 lg:text-lg">Recent Questions</p>
 
-      <ul>
-        {datas && datas.map(data => (
-          <li key={data.id}>
-            <Link href={`/posts/${data.id}`}> 
-              <p>{`◉ ${data.title}`}</p>
-            </Link>
-          </li>
-        ))}
-      </ul>
+          <h2 class="mb-4 text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">最新の質問一覧</h2>
+          <ul>
+            {datas && datas.map(data => (
+              <li key={data.id}>
+                <Link href={`/posts/${data.id}`}> 
+                  <p className='max-w-screen-md text-gray-500 md:text-lg'>{`◉ ${data.created_at} : ${data.title}`}</p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
