df1780f8b86de99495e5e4995fa17a3eafec3a62
大城昂大 <oshirokodai@ooshirodainoMBP.lan>
2023-08-24 11:08:22 +0900

    アロー関数の適用

--- frontend/web/components/layouts/navigation.js
+++ frontend/web/components/layouts/navigation.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-export default function Navigation() {
+const Navigation = () => {
     return (
         <div>
             <header className="flex items-center justify-between py-4 md:py-8">
@@ -8,17 +8,19 @@ export default function Navigation() {
                 <a className="inline-flex items-center gap-2.5 text-2xl font-bold text-indigo-500 md:text-3xl" aria-label="logo">Qikoo</a>
                 </Link>
                  <nav className="hidden gap-12 lg:flex">
-                    <Link href="http://0.0.0.0:8000">
+                    <Link href="/">
                         <a className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">ホーム</a>
                     </Link>
-                    <Link href="">
+                    <Link href="/">
                         <a className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">質問を投稿する</a>
                     </Link>
-                    <Link href="">
+                    <Link href="/">
                         <a className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">ログアウト</a>
                     </Link>
                 </nav>
             </header> 
         </div>
-    );
-}
+    )
+};
+
+export default Navigation;
