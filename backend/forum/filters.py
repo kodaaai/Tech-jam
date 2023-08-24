559fe48f988f9bc6a8a2891dfa60fa9cd0bedfd1
大城昂大 <oshirokodai@ooshirokoudainoMacBook-Pro.local>
2023-08-25 01:14:26 +0900

    不要なファイルの削除

--- backend/forum/filters.py
+++
@@ -1,8 +0,0 @@
-from django_filters import rest_framework as filters
-from .models import Questions
-
-class QuestionFilter(filters.FilterSet):
-
-    class Meta:
-        model = Questions
-        field= ("title", "body")
